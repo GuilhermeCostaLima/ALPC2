16ed6415b9fb91e3c5a756ed1cd3276588b5d665
GuilhermeCostaLima <guilhermeguilhermev2@gmail.com>
2023-08-03 19:37:58 -0300

    Soma

---
+++ EX13struturaS .py
@@ -0,0 +1,16 @@
+'''Faça um programa que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. 
+A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e 
+vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis.'''
+
+
+
+
+a = 10
+b = 20
+
+soma = a + 10
+
+sub = b - 10
+
+print(a)
+print(b)
