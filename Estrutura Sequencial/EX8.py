62cb2c20b2f79ded1cfab0a64ae339efc30749dc
Guilherme <guilhermeguilhermev2@gmail.com>
2023-08-06 23:28:07 -0300

    Combustível gasto em uma viagem

---
+++ Estrutura Sequencial/EX8.py
@@ -0,0 +1,18 @@
+'''8. Faça um programa que calcule a quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12Km por litro. 
+Para obter o cálculo, o usuário deve fornecer o tempo gasto na viagem e a velocidade média durante ela. Desta forma, 
+será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO × VELOCIDADE. Tendo o valor da distância, 
+basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula: LITROS_USADOS = DISTANCIA ÷ 12. 
+O programa deve apresentar os valores da velocidade média, tempo gasto na viagem, a distância percorrida e a quantidade de litros 
+utilizada na viagem.'''
+
+tempo = float(input("Tempo gasto na viagem (em horas): "))
+velo = float(input("Velocidade média durante a viagem (em Km/h): "))
+
+distancia = tempo * velo
+
+litros_usados = distancia / 12
+
+print(f'Velocidade média: {velo}Km/h')
+print(f'Tempo gasto na viagem: {tempo}H')
+print(f'Distancia percorrida: {distancia}Km')
+print(f'Quantidade de litros utilizado: {litros_usados:.2f}L')
