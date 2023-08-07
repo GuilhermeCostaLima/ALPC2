474256d0e49c735aa4e55ef0c6ad808b9bd32cc2
Guilherme <guilhermeguilhermev2@gmail.com>
2023-08-06 23:28:35 -0300

    Consumo médio de um automóvel

---
+++ Estrutura Sequencial/EX9.py
@@ -0,0 +1,15 @@
+'''9. Faça um programa para determinar o consumo médio de um automóvel sendo fornecida a distância total percorrida pelo automóvel 
+e o total de combustível gasto.'''
+
+
+distancia = float(input("Distância total (em Km): "))
+combustivel = float(input("Combustível gasto (em Litros): "))
+
+consumo_medio = distancia / combustivel
+
+print(f'O consúmo médio é de: {consumo_medio} Km/l')
+
+
+
+
+
