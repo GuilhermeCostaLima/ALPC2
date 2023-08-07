ece0c7b48c7b059be4c6a544d6dd4fce94060565
Guilherme <guilhermeguilhermev2@gmail.com>
2023-08-06 23:25:20 -0300

    Nome e sobrenome

---
+++ Estrutura Sequencial/EX6.py
@@ -0,0 +1,18 @@
+'''Faça um programa que:
+
+Leia o nome;
+Leia o sobrenome;
+Concatene o nome com o sobrenome;
+Apresente o nome completo.'''
+
+
+nome1 = input("Primeiro nome: ")
+nome2 = input("Sobrenome: ")
+
+print(f'{nome1.capitalize()} {nome2.capitalize()}')
+
+
+
+
+
+
