c89130ce9938181c07cb9a81b7f752b51c6ba053
Guilherme da Costa de Lima <127263348+GuilhermeCostaLima@users.noreply.github.com>
2023-08-10 19:30:58 -0300

    Posição do r

--- Strings/EXextra.py
+++ Strings/EXextra.py
@@ -7,9 +7,4 @@ while True:
     c = f.find("r",c+1)
     if c == -1:
         break
-<<<<<<< HEAD
     print(f'O r está na posição {c}')
-=======
-    print(f'O r está na posição {c}')
-    
->>>>>>> 51456eb2e3a8ba48849382d8f559e019fb78c9f4
