4e77b4dc62c5283eb2c4c0e41f27032bb6269c7c
GuilhermeCostaLima <guilhermeguilhermev2@gmail.com>
2023-08-17 17:10:46 -0300

    Inverso de uma palavra ou frase

---
+++ Strings/EXextra3.py
@@ -0,0 +1,19 @@
+'''Faça um programa que peça ao usuario uma palavra ou frase,e crie uma segunda string cujo conteudo é o inverso do informado'''
+
+normal = input("palavra:")
+inverso = ""
+for c in normal:
+    inverso = c + inverso
+
+print(normal)
+print(inverso)
+
+
+
+
+
+
+
+
+
+
