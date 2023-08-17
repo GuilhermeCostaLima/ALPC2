f585bfefe1e51475d465e51f998a4d2cf978780b
GuilhermeCostaLima <guilhermeguilhermev2@gmail.com>
2023-08-17 17:06:23 -0300

    Posição do r

--- Strings/EXextra.py
+++ Strings/EXextra.py
@@ -1,15 +1,10 @@
 '''imprima todas as posições onde o caracter 'r' usando find'''
 
 f = "O rato roubou a rica roupa do rei da russia"
-c = 0
+c = -1
 
 while True:
     c = f.find("r",c+1)
     if c == -1:
         break
-<<<<<<< HEAD
     print(f'O r está na posição {c}')
-=======
-    print(f'O r está na posição {c}')
-    
->>>>>>> 51456eb2e3a8ba48849382d8f559e019fb78c9f4
