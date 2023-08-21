2d7d8921d206c295a3d7daea5d915d6b8a483042
GuilhermeCostaLima <guilhermeguilhermev2@gmail.com>
2023-08-21 15:55:53 -0300

    Soma dos números inteiros de 1 a 100

---
+++ Estrutura de Repetição/EX-1.py
@@ -0,0 +1,8 @@
+'''1.Faça um programa que calcule a soma dos números inteiros de 1 a 100.'''
+
+numero = 0 
+soma = 0
+while numero < 100:
+    numero += 1
+    soma += numero
+print(f'A soma dos inteiros é: {soma}')
